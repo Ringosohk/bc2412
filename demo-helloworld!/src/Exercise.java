d919a03ac724aa5ec17f95b5aec6031fefee9f77
Ringo <ringoskm@gmail.com>
2024-12-10 13:03:17 +0800

    20241210

---
+++ demo-helloworld!/src/Exercise.java
@@ -0,0 +1,23 @@
+public class Exercise {
+
+public static void main (String[]args){
+  double priceForApple = 7.3;
+  double priceForOrange = 6.5;
+  int quantityOfApple = 3;
+  int quantityOfOrange = 4;
+
+  double totalprice = priceForApple * quantityOfApple + priceForOrange * quantityOfOrange;
+
+  System.out.println(totalprice);
+
+
+  int mathScore = 73;
+  int englishScore = 68;
+  int historyScore = 61;
+
+  int averageScore = (mathScore + englishScore +historyScore)/3;
+
+  System.out.println(averageScore);
+
+}
+}
